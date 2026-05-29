cc98900711c97fde7ec28639ffff8e50af2fdf6c
lamaxime20 <lamaxime20@gmail.com>
2026-05-29 05:50:22 +0200

    Ajoute d'un nouveau lien

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,7 +22,7 @@ return [
         'trim',
         explode(',', env(
             'CORS_ALLOWED_ORIGINS',
-            'http://localhost:5173,http://127.0.0.1:5173,https://agora-app-rouge.vercel.app'
+            'http://localhost:5173,http://127.0.0.1:5173,https://agora-app-rouge.vercel.app,https://agora-app-1.onrender.com'
         ))
     ))),
 
