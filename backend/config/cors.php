f2ffa4c2f6cc591984c28b4d1384c7382569101d
lamaxime20 <lamaxime20@gmail.com>
2026-05-29 05:36:31 +0200

    Police cors mis à jour

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,6 +20,8 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://agora-app-rouge.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
